48be4f847cefc22d1bde45150684e6799562185f
SPY_001 <45824217+SPY001720@users.noreply.github.com>
2018-12-21 17:05:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ bot.on('ready', () => {
     bot.user.setPresence({ game: { name: '[!Mystik] <🔱 𝓢𝓟𝓨 🔱>', type: 0}})
     console.log("Bot pret");
 });
-
+ 
         bot.on('message', message => {
-            if (message.content === "!mention"){
-                message.reply("")
+            if (message.content === "!plainte"){
+                message.reply("Merci de votre plainte , nous allons la traité dès que possible ! ✅")
             }
+
+            bot.on('message', message => {
+                if (message.content === "!mention"){
+                    message.reply("")
+                }
 
         if (message.content === prefix+ "Mystik"){
             var help_embed = new Discord.RichEmbed()
@@ -22,7 +27,7 @@ bot.on('ready', () => {
                 .addField("!infos", "Cette commande permet de vous informer sur le serveur.")
                 .addField("!staff", "Cette commande est utile si vous avez besoins de parler a un staff d'un certain grade , elle permet d'afficher la liste des staff de MystikRP ainsi que leur grade.")
                 .addField("!maj", "Cette commande permet de voir les mises à jours prévus ou déjâ faites sur MystikRP !")
-                .addField("!liens", "Cette commande vous affiches les liens utiles")
+                .addField("!plainte", "Cette commande permet de déposer une plainte contre un joueur : !plainte <message de plainte")
             message.channel.sendEmbed(help_embed);
         }
 
@@ -34,10 +39,10 @@ bot.on('ready', () => {
                 .addField("!infos", "Cette commande permet de vous informer sur le serveur.")
                 .addField("!staff", "Cette commande est utile si vous avez besoins de parler a un staff d'un certain grade , elle permet d'afficher la liste des staff de MystikRP ainsi que leur grade.")
                 .addField("!maj", "Cette commande permet de voir les mises à jours prévus ou déjâ faites sur MystikRP !")
-                .addField("!liens", "Cette commande vous affiches les liens utiles")
+                .addField("!plainte", "Cette commande permet de déposer une plainte contre un joueur : !plainte <message de plainte")
             message.channel.sendEmbed(help_embed);
-        }            
-            
+        }
+
         if (message.content === prefix+ "infos"){
             var help_embed = new Discord.RichEmbed()
                 .setColor('#AEEE00')
@@ -49,23 +54,11 @@ bot.on('ready', () => {
                 .addField("Site Web :", "https://loloxcommunity.com/")
             message.channel.sendEmbed(help_embed);
         }
-            
-        if (message.content === prefix+ "liens"){
-            var help_embed = new Discord.RichEmbed()
-                .setColor('#AEEE00')
-                .addField("Liens utiles :", "Les liens utiles pour MystikRP sont ici !")
-                .addField("Workshop MystikRP :", "https://steamcommunity.com/sharedfiles/filedetails/?id=1184169554")
-                .addField("Discord MystikRP :", "https://discord.gg/JuhH6mZ")
-                .addField("Chaîne YouTube Lolox :", "https://www.youtube.com/channel/UCQmyH_HI5Sl41oBqGw7o7gw")
-                .addField("Chaîne Twitch Lolox :", "https://www.twitch.tv/lolox_fr")
-                .addField("Site Web :", "https://loloxcommunity.com/")
-            message.channel.sendEmbed(help_embed);
-        }            
  
         if (message.content === prefix+ "maj"){
             var help_embed = new Discord.RichEmbed()
                 .setColor('#AEEE00')
-                .addField("Mises à jours au total :"," Il y a **3** mises à jours au total !")
+                .addField("Mises à jours au total :"," **3**")
                 .addField("2.2", "**-Optimisation des addons + ajout d'une préconfig pour améliorer la fluidité et réduire la perte d'fps**\n **-Remise en fonction du métier entrepreneur**\n **-Modification des grades de gendarmes**\n **-Modification des skins de différent jobs**\n **-Debug de plusieurs addons**\n **-Debug du ban plus importation des règles en jeu**\n **-Amélioration des performance du serveur**\n **-Ajout de munition directement dans les armes du serveur en tant qu'agent de la bac policier , gign etc ....**\n **-Modification de certains véhicules**\n **-Debug de l'addon maire**\n **-Ajout du métier fermier + pêcheur**")
                 .addField("2.3", "**-Optimisation des addons + Boost FPS**\n **-Nerf de certains métier**\n **-Modification des grades de gendarmes**\n **-Modification de la map**\n **-Modification du systeme de vente d'appartement**\n **-Amélioration des performance du serveur**\n **-Debug de l'addon maire**\n **-Ajout du métier fermier (Animaux demain)**\n **-Ajout d'un nouveau systeme pénitencier (SOON)**")
                 .addField("2.4 (Future MAJ)", "**-Modification des véhicules possédant des sirènes + ajout du VCMOD ELS**\n **-Ajout des plaques d'immatriculation**\n **-Ajout des Animaux (Normalement)**\n **-Ajout dans les warn des sanctions déjà crée** \n **-Optimisation du Worshop** \n **-Optimisation du join au serveur** \n **-Optimisation des Addons** \n **-Stabilisation du serveur**\n **-Suppression du Nom Roleplay + de la vie + Changement de métier**\n **-Traduction du Darkrp** \n **-Ajout de deux nouveaux métiers mystère !**\n **-Réglages de certains beugs** \n **-Ajout de munitions lors du spawn en gendarme ou autres métiers** \n **-Ajout de nouveaux grades pour la police , gign**\n **-Ajout de grades pour l'agent de sécurité** \n **-Ajout de grades pour le Pompier**\n **-Ajout de grades pour le Médecin **")
@@ -79,7 +72,7 @@ bot.on('ready', () => {
                 .addField("Fondateur", "Lolox")
                 .addField("Mappeur", "Pelisma")
                 .addField("Superadmins", "Kobralost\n Stylaxx")
-                .addField("Administrateurs", "Skyflix\n Alex Backo\n El Famoso Marvino\n 🔱 𝓢𝓟𝓨 🔱\n ChickenNuggets (Hoie)\n A Fromage de chevre")
+                .addField("Administrateurs", "Skyflix\n Alex Backo\n El Famoso Marvino\n 🔱 𝓢𝓟𝓨 🔱\n ChickenNuggets (Hoie)\n A Fromage de chevre\n Petit enfant arrogant")
             message.channel.sendEmbed(help_embed);
         }
 
